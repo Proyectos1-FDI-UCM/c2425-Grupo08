80e05f44013f4450a2e83fc15cd2f043cd171daa
car-dochao <carlos.dochao@gmail.com>
2025-04-25 09:33:54 +0200

    Hotfix InputManager
    Se devuelve correctamente el string del input que se solicita
    
    + Ligeros ajustes a la fuente pixel art para que se lea mejor

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -6,6 +6,7 @@
 //---------------------------------------------------------
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Linq;
 
 /// <summary>
 /// Manager para la gestión del Input. Se encarga de centralizar la gestión
@@ -209,30 +210,30 @@ public class InputManager : MonoBehaviour
     }
 
     // Getters para los controles del juego
-    
+
     public string GetInteractKey()
     {
-        return _interact.bindings[0].effectivePath;
+        return _interact.bindings[0].effectivePath.Split('/').Last().ToUpper();
     }
 
     public string GetJumpKey()
     {
-        return _jump.bindings[0].effectivePath;
+        return _jump.bindings[0].effectivePath.Split('/').Last().ToUpper();
     }
 
     public string GetFocusKey()
     {
-        return _focus.bindings[0].effectivePath;
+        return _focus.bindings[0].effectivePath.Split('/').Last().ToUpper();
     }
 
     public string GetFlashKey()
     {
-        return _flash.bindings[0].effectivePath;
+        return _flash.bindings[0].effectivePath.Split('/').Last().ToUpper();
     }
 
     public string GetReturnKey()
     {
-        return _return.bindings[0].effectivePath;
+        return _return.bindings[0].effectivePath.Split('/').Last().ToUpper();
     }
 
     #endregion
